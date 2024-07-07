fb347d6061ca3f68c2bcb985051637cd69430dd3
nihal shameem <nihalshameem@APPLEs-MacBook-Pro.local>
2024-07-07 11:35:27 +0530

    build bug fix and fonts bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,css}', // Include your app directory and CSS files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
